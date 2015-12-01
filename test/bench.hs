0d30c6c571a98531513bd1be9b9bd09bfe5d2276
Sharif Olorin <sio@tesser.org>
2015-12-01 23:59:43 +0000

    Make comparison benchmarks more representative
     - Copy bytestring rather than comparing bytestring to itself -
       Data.ByteString.Internal.eq will shortcut the comparison if
       both arguments point to the same address in memory.
     - Use $! in unsafe wrapper - doesn't actually evaluate without it.

--- test/bench.hs
+++ test/bench.hs
@@ -9,6 +9,7 @@ import           Data.ByteString (ByteString)
 import qualified Data.ByteString as BS
 
 import           P
+import           Prelude (($!))
 
 import           System.IO
 
@@ -18,15 +19,17 @@ import           Test.QuickCheck.Instances ()
 import           Tinfoil.Random
 import           Tinfoil.KDF
 
-hashPair :: Int -> Int -> Gen (ByteString, ByteString)
-hashPair small big = do
+bsTriple :: Int -> Int -> Gen (ByteString, ByteString, ByteString)
+bsTriple small big = do
   short1 <- vectorOf small arbitrary
   short2 <- vectorOf small arbitrary `suchThat` (/= short1)
   long <- vectorOf big arbitrary
-  pure (BS.pack $ short1 <> long, BS.pack $ short2 <> long)
+  let big1 = BS.pack $ short2 <> long
+  let big2 = BS.copy big1
+  pure (BS.pack $ short1 <> long, big1, big2)
 
-unsafeEq :: (ByteString, ByteString) -> Bool
-unsafeEq = uncurry (==)
+unsafeEq :: ByteString -> ByteString -> IO Bool
+unsafeEq a b = pure $! a == b
 
 main :: IO ()
 main = defaultMain [
@@ -35,10 +38,10 @@ main = defaultMain [
       , bench "1000" $ nfIO (randomCredential [] 1000)
       , bench "100000" $ nfIO (randomCredential [] 100000)
       ]
-  , env (generate $ hashPair 5 1000) $ \ ~(h1, h2) ->
-      bgroup "eqs/large" $ [ bench "safe/same" $ nfIO (safeEq h2 h2)
-                           , bench "safe/different" $ nfIO (safeEq h1 h2)
-                           , bench "unsafe/same" $ nf unsafeEq (h2, h2)
-                           , bench "unsafe/different" $ nf unsafeEq (h1, h2)
+  , env (generate $ bsTriple 5 10000) $ \ ~(h1, h2_1, h2_2) ->
+      bgroup "eqs/large" $ [ bench "safe/same" $ nfIO (safeEq h2_1 h2_1)
+                           , bench "safe/different" $ nfIO (safeEq h1 h2_1)
+                           , bench "unsafe/same" $ nfIO (unsafeEq h2_1 h2_2)
+                           , bench "unsafe/different" $ nfIO (unsafeEq h1 h2_1)
                            ]
   ]
