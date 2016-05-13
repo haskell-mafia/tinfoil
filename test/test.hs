2bc24d4f6d12b51a9ff347ecd01f4d405b647a75
Sharif Olorin <sio@tesser.org>
2016-05-13 07:29:11 +0000

    Add NESSIE test vectors for SHA256

--- test/test.hs
+++ test/test.hs
@@ -3,6 +3,7 @@ import           Disorder.Core.Main
 import qualified Test.Tinfoil.Data.KDF
 import qualified Test.Tinfoil.Data.Signing
 import qualified Test.Tinfoil.Hash
+import qualified Test.Tinfoil.Hash.TestVectors
 import qualified Test.Tinfoil.Random
 import qualified Test.Tinfoil.KDF.Scrypt
 
@@ -12,6 +13,7 @@ main =
     Test.Tinfoil.Data.KDF.tests
   , Test.Tinfoil.Data.Signing.tests
   , Test.Tinfoil.Hash.tests
+  , Test.Tinfoil.Hash.TestVectors.tests
   , Test.Tinfoil.Random.tests
   , Test.Tinfoil.KDF.Scrypt.tests
   ]
