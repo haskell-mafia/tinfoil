ac9dc78a543c0128bdf39336d17f882349e659d7
Sharif Olorin <sio@tesser.org>
2016-06-03 01:25:28 +0000

    Render hash digests

--- src/Tinfoil/Data/Hash.hs
+++ src/Tinfoil/Data/Hash.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE DeriveGeneric #-}
 module Tinfoil.Data.Hash(
     Hash(..)
+  , renderHash
 ) where
 
 import           Control.DeepSeq.Generics (genericRnf)
@@ -13,6 +14,8 @@ import           GHC.Generics (Generic)
 
 import           P
 
+import           Tinfoil.Encode
+
 -- | Binary representation of a hash.
 newtype Hash =
   Hash {
@@ -20,3 +23,6 @@ newtype Hash =
   } deriving (Eq, Show, Generic)
 
 instance NFData Hash where rnf = genericRnf
+
+renderHash :: Hash -> Text
+renderHash = hexEncode . unHash
