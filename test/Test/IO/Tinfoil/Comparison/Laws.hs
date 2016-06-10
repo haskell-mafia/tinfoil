847651882795c4132d322fb1dcafa416b8246d23
Sharif Olorin <sio@tesser.org>
2016-06-10 08:41:40 +0000

    More ConstEq laws

--- test/Test/IO/Tinfoil/Comparison/Laws.hs
+++ test/Test/IO/Tinfoil/Comparison/Laws.hs
@@ -13,3 +13,16 @@ constEqId :: (ConstEq a) => a -> Property
 constEqId x = testIO $ do
   r1 <- x ==# x
   pure $ r1 === True
+
+constEqTrans :: (ConstEq a) => a -> a -> a -> Property
+constEqTrans x y z = testIO $ do
+  xy <- x ==# y
+  yz <- y ==# z
+  xz <- x ==# z
+  pure $ (xy && yz) ==> (xz === True)
+
+constEqEq :: (Eq a, ConstEq a) => a -> a -> Property
+constEqEq x y = testIO $ do
+  r1 <- x ==# y
+  let r2 = x == y
+  pure $ r1 === r2
--- test/Test/IO/Tinfoil/Data/MAC.hs
+++ test/Test/IO/Tinfoil/Data/MAC.hs
@@ -4,6 +4,8 @@
 
 module Test.IO.Tinfoil.Data.MAC where
 
+import qualified Data.ByteString.Char8 as BSC
+
 import           P
 
 import           System.IO
@@ -14,8 +16,20 @@ import           Test.IO.Tinfoil.Comparison.Laws
 import           Test.Tinfoil.Arbitrary ()
 import           Test.QuickCheck
 
-prop_ConstEqId :: MAC -> Property
-prop_ConstEqId = constEqId
+prop_MAC_ConstEqId :: MAC -> Property
+prop_MAC_ConstEqId = constEqId
+
+prop_MAC_ConstEqEq :: MAC -> MAC -> Property
+prop_MAC_ConstEqEq = constEqEq
+
+-- | Can't use real random bytestrings here as the probability of three
+-- identical values is tiny.
+prop_MAC_ConstEqTrans :: Bool -> Bool -> Bool -> Property
+prop_MAC_ConstEqTrans x y z =
+  let x' = MAC . BSC.pack $ show x
+      y' = MAC . BSC.pack $ show y
+      z' = MAC . BSC.pack $ show z in
+  constEqTrans x' y' z'
 
 return []
 tests :: IO Bool
