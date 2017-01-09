ee0393b50445885517566f0477bb2eadba1846fe
Sharif Olorin <sio@tesser.org>
2017-01-09 06:25:14 +0000

    aes-gcm: generate/increment invocation field

--- src/Tinfoil/AEAD/AESGCM/Iv.hs
+++ src/Tinfoil/AEAD/AESGCM/Iv.hs
@@ -2,4 +2,31 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 
 module Tinfoil.AEAD.AESGCM.Iv (
+    IvError(..)
+
+  , newInvocationField
+  , incrementInvocationField
   ) where
+
+import           P
+
+import           System.IO (IO)
+
+import           Tinfoil.AEAD.AESGCM.Data
+import           Tinfoil.Random
+
+data IvError =
+    InvocationCountOverflow
+
+newInvocationField :: IO InvocationField
+newInvocationField =
+  InvocationField <$> (RandomField <$> randomWord32) <*> (pure $ InvocationCount 0)
+
+incrementInvocationField :: InvocationField -> Either IvError InvocationField
+incrementInvocationField (InvocationField r (InvocationCount n)) =
+  let
+    n' = n + 1
+  in
+  case n' > ((2 :: Integer)^(32 :: Integer) - 1) of
+    True -> Left InvocationCountOverflow
+    False -> Right . InvocationField r $ InvocationCount n'
