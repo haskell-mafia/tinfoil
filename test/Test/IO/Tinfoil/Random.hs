adc2e931031cff3c6b923333329bd2a5543fd57d
Sharif Olorin <sio@tesser.org>
2017-07-13 05:12:11 +0000

    Test urandom implementation

--- test/Test/IO/Tinfoil/Random.hs
+++ test/Test/IO/Tinfoil/Random.hs
@@ -15,6 +15,7 @@ import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
 
 import           Disorder.Core.IO
+import           Disorder.Core.Property ((=/=))
 
 import           P
 
@@ -22,6 +23,7 @@ import           System.IO
 
 import           Tinfoil.Data
 import           Tinfoil.Random
+import           Tinfoil.Random.Internal
 
 import           Test.QuickCheck
 import           Test.Tinfoil.Arbitrary ()
@@ -51,6 +53,23 @@ prop_drawOnce_closure :: Property
 prop_drawOnce_closure = forAll arbitrary $ \(xs :: NonEmpty Int) -> testIO $ do
   x <- drawOnce xs
   pure $ elem x xs
+
+mi :: Int
+mi =
+  1024 * 1024
+
+prop_urandom_length :: Property
+prop_urandom_length =
+  forAll (choose (0, 10 * mi)) $ \n -> testIO $ do
+    bs <- urandom n
+    pure $ BS.length bs === n
+
+prop_urandom_neq :: Property
+prop_urandom_neq =
+  forAll (choose (128, 10 * mi)) $ \n -> testIO $ do
+    bs1 <- urandom n
+    bs2 <- urandom n
+    pure $ bs1 =/= bs2
 
 return []
 tests :: IO Bool
