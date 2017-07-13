cdab995d9749359f4c279c9310f4d2082e0b6937
Sharif Olorin <sio@tesser.org>
2017-07-13 05:03:41 +0000

    Add urandom implementation for entropy

--- src/Tinfoil/Random/Internal.hs
+++ src/Tinfoil/Random/Internal.hs
@@ -6,6 +6,7 @@ module Tinfoil.Random.Internal(
   , readBitsLE
   , dropMS
   , segmentsOf
+  , urandom
 ) where
 
 import           Data.Bits
@@ -13,6 +14,9 @@ import           Data.ByteString (ByteString)
 import qualified Data.ByteString as BS
 
 import           P
+
+import           System.IO (IOMode(..), IO)
+import           System.IO (withFile)
 
 segmentsOf :: Int -> [a] -> [[a]]
 segmentsOf _ [] = []
@@ -33,3 +37,20 @@ readBitsLE = foldr' pack 0
 
 explodeBS :: ByteString -> [Bool]
 explodeBS = concatMap (\w -> testBit w <$> [0..7]) . BS.unpack
+
+-- |
+-- Read entropy from /dev/urandom.
+--
+-- On OS X this calls a 160-bit Yarrow generator and will block until
+-- the entropy pool has been initialised (should never happen); on
+-- Linux it is a sha1-based generator and will not block regardless of
+-- pool state.
+--
+-- This function incurs some overhead due to GHC.IO buffering; if this
+-- becomes significant (e.g., key generation), using read(3) directly
+-- would make small reads significantly faster.
+--
+-- FIXME: is it worth checking uname and failing on non-linux/darwin here?
+urandom :: Int -> IO ByteString
+urandom n =
+  withFile "/dev/urandom" ReadMode (\h -> BS.hGet h n)
